c555fc3d81a98e93aad73d703d7d039177093670
hyeonhyeon-K <jklgus08@gmail.com>
2023-05-23 23:45:25 +0900

    test

--- app/page.js
+++ app/page.js
@@ -11,8 +11,6 @@ export default function Home() {
         </p>
         <div>
           <a
-
-          
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
