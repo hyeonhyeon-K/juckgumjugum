aa9687810333919ab4fef53f4fa348e721207888
hyeonhyeon-K <jklgus08@gmail.com>
2023-05-27 18:32:08 +0900

    NEXT-10 css 예금 메인페이지

---
+++ app/deposit/DayDeposit.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { FaAngleRight } from "react-icons/fa";
+
+export default function dayDeposit(){
+    return(
+        <div className="dayDepositBox">
+                <div className="dayDeposit">당좌예금</div>
+                <FaAngleRight/>
+        </div>
+    )
+}
---
+++ app/deposit/Explan.js
@@ -0,0 +1,16 @@
+'use client'
+
+export default function Expalne(){
+  return(
+    <div className="explanbody">
+        <div className="expalnebox">
+            <p id="explaneTitle">예금이란?</p>
+            <p id="explaneContent">
+            일정 금액을 일정 기간 동안 금융 기관에 맡기고 정한 기한 안에는 찾지 않겠다는 것으로 이자가 높다.
+            </p>
+        </div>
+    </div>
+
+    
+  )
+}
