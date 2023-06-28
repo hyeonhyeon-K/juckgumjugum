c71cd87fbe2a2520267f6e354197feeaa248cca9
hyeonhyeon-K <jklgus08@gmail.com>
2023-06-28 18:25:29 +0900

    마이페이지 완

--- app/mypage/buylist/Buylist.js
+++ app/mypage/buylist/Buylist.js
@@ -1,11 +1,13 @@
 'use client'
-import { FaRegBell, FaShoppingBasket } from "react-icons/fa";
+import { FaShoppingBasket } from "react-icons/fa";
 import Image from 'next/image'
 import React from "react"
-import cuk from "../../../public/국민은행.png"
+import {useRouter} from 'next/navigation'
 
 export default function Mypage(result){
     const mypagemap = result.result
+    const router = useRouter()
+
   return(
     <div>
             <div className="mypageTitleBox">
@@ -14,10 +16,10 @@ export default function Mypage(result){
             </div>
             <div className="mypageContent">
                 <div className="mypageSideBar">
-                    <div className="mypageSidebarC">
+                    {/* <div className="mypageSidebarC">
                         <FaRegBell id="mypageIcon"/>
                             알림함 가기
-                        </div>
+                        </div> */}
                     {/* <div className="mypageSidebarC">
                         <FaRegUser id="mypageIcon"/>
                             회원정보 변경
@@ -31,34 +33,11 @@ export default function Mypage(result){
                 <div className="buyListBoxBody">
                     <div className='buyListBox'>
                         
-                        <div className="buyListflex">
-                        <Image src={cuk} alt={`${result.bank}`} width="55" height="55" className='shopListImg'/>
-                    
-                        <div className='shopList'>
-                                <div className='buyListTag'>
-                                    <div className='shopListTagD'>tag</div>
-                                    <div className='shopListTagD1'>tag</div>
-                                    <div className='shopListTagD1'>tag</div>
-                                </div>
-                                <div className='shopListContent'>
-                                    <div className='buyListContentTitle'>예시적금</div>
-                                    <div className='shopListContentC'>희망찬 내일을 위해 희망찬 미래를 위해 희망의 우리를 위해</div>
-                                    <a className='shopListContentNum'>4.2</a>
-                                    <div className='buylistContentExBox'>
-                                        <div className='buyListContentEx'>최고연</div>
-                                        <div className='buyListContentPer'>%</div>
-                                    </div>
-                                    <div className='buyContentExBox'>
-                                        <div className='buyContentEx1'>기본 2.45%</div>
-                                        <div className='buyContentEx1Ex'>(24개월)</div>
-                                    </div>
-                                </div>
-                        </div>
-                        </div>
                         {/* 여기서부터 아래 표시까지 map 걸면 댐 */}
 
                         {
-                            mypagemap.map((a)=>(
+                            mypagemap.length === 0 ? <div className="buylistAlarm">등록한 적금,예금이 없습니다.</div>
+                            :  mypagemap.map((a)=>(
                                 <>
                                 <div className="buyListFlexBox" key={a}/>
                                 <div className="buyListflex">
@@ -84,6 +63,13 @@ export default function Mypage(result){
                                             </div>
                                         </div>
                                 </div>
+                                <form className="buyListDeleteForm">
+                                    <span onClick={()=>(
+                                        fetch('/api/delete', {method : 'DELETE', _id: a._id}).then(()=>{
+                                            router.refresh()
+                                        }))}
+                                        className="buyListDelete">삭제</span>
+                                </form>
                                 </div>
                                 </>
                             ))
--- app/mypage/buylist/page.js
+++ app/mypage/buylist/page.js
@@ -1,11 +1,15 @@
 import { connectDB } from "../../util/database.js"
 import BuyList from "./Buylist"
 import React from "react"
-
+import { getServerSession } from "next-auth";
+import {authOptions} from '../../../pages/api/auth/[...nextauth].js'
 
 export default async function Buylist(){
+    const session = await getServerSession(authOptions)
+    const userEmail = session.user.email
     const db = (await connectDB).db('jukgum');
-    const result = await db.collection('get').find().toArray();
+    const result = await db.collection('get').find({ author : userEmail }).toArray();
+
     return(
         <div>
             <BuyList result={result}/>
