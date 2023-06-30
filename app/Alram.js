3df14d3175ae79bd1632a87613a045ee34c7cfc4
hyeonhyeon-K <jklgus08@gmail.com>
2023-06-30 16:27:39 +0900

    알림 완 useEffect

---
+++ app/Alram.js
@@ -0,0 +1,33 @@
+"use client"
+
+import React, {useEffect, useState} from "react"
+
+
+
+export default function Header(){
+  const [alert, setAlert] = useState(true)
+  useEffect(() => {
+    const timer = setTimeout(()=>{setAlert(false)}, 2500);
+    return ()=> {clearTimeout(timer)}
+  }, []);
+
+
+    return(
+        <div>
+            {
+              alert === true ? (
+                <div className="alramDiv">
+                <div className="alramBody">
+                    <div className="alramHeader">
+                        <div className="alramTitle">알림</div>
+                    </div>
+                    <div className="alramContent">반가워요! 글을 작성해 <br/> 보시겠어요?</div>
+                </div>
+            </div>
+              ) : null
+            }
+            
+        </div>
+        
+    )
+}
