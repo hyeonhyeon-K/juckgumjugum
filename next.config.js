a5b75a134a0b566308faa78e44a4050645f39095
hyeonhyeon-K <jklgus08@gmail.com>
2023-07-02 15:30:39 +0900

    sharp Image 최적화

--- next.config.js
+++ next.config.js
@@ -3,14 +3,13 @@ const nextConfig = {
     experimental: {
         appDir: true,
       },
-      // async rewrites() {
-      //   return [
-      //     {
-      //       source: '/finlifeapi/',
-      //       destination: 'https://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=855905c0fb40765d5f0ee1cf2746ce6c&topFinGrpNo=020000&pageNo=1', // 실제 API 엔드포인트
-      //     },
-      //   ];
-      // }
+      webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+      ) => {
+        // Important: return the modified config
+        return config
+      },
 }
 
 module.exports = nextConfig
