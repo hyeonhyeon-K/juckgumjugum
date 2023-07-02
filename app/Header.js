a68e719a6e76b7d357cebd1b5ac06df2aee79618
hyeonhyeon-K <jklgus08@gmail.com>
2023-07-02 14:39:15 +0900

    location -D

--- app/Header.js
+++ app/Header.js
@@ -3,18 +3,14 @@
 import { useRouter } from "next/navigation";
 import {signIn} from "next-auth/react"
 // import { FaSearch } from "react-icons/fa";
-import React, {useEffect} from "react"
+import React from "react"
 import Alram from "./Alram"
 
 
 
 export default function Header(){
   let router = useRouter()
-  useEffect(()=>{
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position)
-    });
-  },[])
+
  
     return(
         <header>
