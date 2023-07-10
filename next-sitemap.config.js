c794eb8c3c7ed7fb3e971f610b518f4277cd2eff
hyeonhyeon-K <jklgus08@gmail.com>
2023-07-11 01:53:13 +0900

    google

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: "https://juckgum.comL",
+    generateRobotsTxt: true,
+    exclude: ['/admin', '/login'],
+    sitemapSize: 7000,
+    changefreq: "daily",
+    priority: 0.5,
+  };
+  
